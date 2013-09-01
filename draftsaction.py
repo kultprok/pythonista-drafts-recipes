e80858c158c0d1e012ed507d943648bb0f6c2655
kultprok <info@kulturproktologie.de>
2013-09-01 17:52:37 +0200

    added manual template to draftsaction.py.

--- draftsaction.py
+++ draftsaction.py
@@ -4,24 +4,34 @@ from os import path
 import sys
 import types
 from urllib import quote, unquote
-
+ 
 action_markdown = '''
 # {name}
 =========================
-
+ 
 # The Drafts Action
-
+ 
 Here's the code for the action. It assumes that the Python scripts is in Pythonista's Documents folder and the script's title is *{name}*.
-
+ 
     {action}
 	
 # Import Link
-
+ 
 If you're on an iOS device and have Drafts installed, you can spare yourself the effort of manually creating the action in Drafts. Just click on the following link:
-
+ 
 [Import action to Drafts]({importlink})
 '''
-
+ 
+manual_markdown = '''
+# Manual
+ 
+[[Instructions here]]
+ 
+# Remarks
+ 
+[[Remarks here]]
+'''
+ 
 class DraftsAction():
 	def __init__(self, title='', scriptpath=''):
 		if scriptpath == '':
@@ -43,11 +53,12 @@ class DraftsAction():
 		self.data = {}
 		self.data_update()
 		
+		
 	def __str__(self):
 		self.data_update()
 		return str(self.data)
-
-
+ 
+ 
 	def _name_of_script(self):
 		'''
 		Get the name of current file.
@@ -57,16 +68,16 @@ class DraftsAction():
 		basename = path.basename(self._scriptpath)
 		scriptname = path.splitext(basename)
 		return scriptname[0]
-
-
+ 
+ 
 	def _path_of_script(self):
 		'''
 		Return the path to the script file.
 		'''
 		absolutepath = path.abspath(self._scriptpath)
 		return path.split(absolutepath)[0]
-
-
+ 
+ 
 	def drafts_action(self, *args):
 		'''
 		A url scheme to a Pythonista script.
@@ -80,8 +91,8 @@ class DraftsAction():
 		self.action = scheme.format(script=self.name,
 	                     args=argstring)
 		self.data_update()
-
-
+ 
+ 
 	def drafts_import_link(self):
 		'''
 		A link for Drafts to import an action.
@@ -91,16 +102,19 @@ class DraftsAction():
 		self.importlink = scheme.format(name=quote(self.actiontitle),
 	                  url=quote(self.action))
 	 	self.data_update()
-
-
+ 
+ 
 	def data_update(self):
+		'''
+		Update the data dictionary.
+		'''
 		self.data =  {'action': self.action,
 		              'actiontitle': self.actiontitle,
 		              'importlink': self.importlink,
 		              'name': self.name,
 		              'path': self.absolutepath}
 	        
-
+ 
 	def create_action_file(self):
 		'''
 		Create a markdown file with Drafts action 
@@ -111,3 +125,13 @@ class DraftsAction():
 		return action_markdown.format(name=self.name,
 		                             action=self.action,
 		                             importlink=self.importlink)
+ 
+ 
+	def create_manual(self):
+		'''
+		Create a markdown manual template in 
+		the scripts directory.
+		'''
+		if self.action == '' or self.importlink == '':
+			raise ValueError("Make sure you've defined an action and an import link.")
+		return manual_markdown
